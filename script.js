b44a2915d2ab502b6cc1e28d4f9c723ab5798f6a
frezonline <55662236+frezonline@users.noreply.github.com>
2021-05-31 14:44:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ const inputValue = document.querySelector(selectors.inputValue);
 
 const htmlOfNote = (text) => `<li>${text} <span class='close'>×</span> </li>`;
 
+function saveToDoList(text) {
+  const todo = { task: text };
+  todoStorage.push(todo);
+  localStorage.setItem("TODO storage", JSON.stringify(todoStorage));
+}
+
+const todoStorage = JSON.parse(localStorage.getItem("TODO storage")) || [];
+todoStorage.forEach((item) => {
+  myUL.insertAdjacentHTML("beforeEnd", htmlOfNote(item.task));
+});
+
 clickAddBtn.addEventListener("click", function addNewNode() {
   const inputValue = document.querySelector("input").value;
   if (inputValue === "") {
@@ -23,7 +34,8 @@ clickAddBtn.addEventListener("click", function addNewNode() {
     return;
   }
 
-  const htmlOfNote = (text) => `<li>${text} <span class='close'>×</span> </li>`;
+  saveToDoList(inputValue);
+
   myUL.insertAdjacentHTML("beforeEnd", htmlOfNote(inputValue));
   document.querySelector("input").value = "";
 });
@@ -42,12 +54,14 @@ listChecked.addEventListener(
 deleteAllBtn.addEventListener("click", function (event) {
   event.target.classList.contains("deliteBtn");
   myUL.innerHTML = "";
+  localStorage.clear(todoStorage);
 });
 
 enterInput.onkeypress = function (event) {
   if (event.keyCode == 13 || event.key == 13) {
     const text = document.querySelector(selectors.inputValue).value;
     myUL.insertAdjacentHTML("beforeEnd", htmlOfNote(text));
+    saveToDoList(text);
     document.querySelector("input").value = "";
   }
 };
